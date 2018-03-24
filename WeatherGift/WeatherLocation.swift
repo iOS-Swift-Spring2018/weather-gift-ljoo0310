99240b09ce15a9b9950e3b5bd570241fda38ebc4
Yehoon <yehoon@yehoons-macbook-pro.local>
2018-03-24 16:46:42 -0400

    7.10

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Yehoon on 3/24/18.
+//  Copyright © 2018 Yehoon Joo. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name = ""
+    var coordinates = ""
+}
